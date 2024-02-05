18b68d9bacb7406baf62ca28f56fbe9d94e62d68
umeshkumawat <umesh@swipepages.com>
2024-02-05 22:03:20 +0530

    gateways init

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/customer', proxy('http://localhost:8001'));
+app.use('/shopping', proxy('http://localhost:8003'));
+app.use('/', proxy('http://localhost:8002')) // products
+
+
+app.listen(8000, () => {
+  console.log('Gateways is running on port number: 8000');
+})
